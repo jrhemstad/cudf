5ce55440a7e03a9ed8fc033239dffabf40cb8a3d
Jake Hemstad <jhemstad@nvidia.com>
2020-04-16 21:20:55 -0500

    Allow configuring sort log file.

--- cpp/src/sort/log.cpp
+++ cpp/src/sort/log.cpp
@@ -15,21 +15,31 @@ inline std::string get_type_name(cudf::data_type type) {
 }  // namespace
 
 std::unique_ptr<cudf::process_range> log_sort(cudf::table_view t) {
+  std::string log_filename{"sort_log.txt"};
+
+  if (nullptr != std::getenv("SORT_LOG_FILE")) {
+    log_filename = std::string{std::getenv("SORT_LOG_FILE")};
+  }
+
   static std::shared_ptr<spdlog::logger> logger =
       std::make_shared<spdlog::logger>(
           "SORT", std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-                      "sorts.txt", true));
+                      log_filename, true));
+
 
   if (t.num_columns() == 1 and not t.column(0).has_nulls()) {
     std::string msg{"single column sort without nulls. "};
-    msg += " query: ";
-    msg += std::getenv("QUERY_NUMBER");
-    msg += " Size: "
+    if (std::getenv("QUERY_NUMBER")) {
+      msg += " query: ";
+      msg += std::getenv("QUERY_NUMBER");
+    }
+    msg += " Size: ";
     msg += std::to_string(t.num_rows());
     msg += " type: ";
     msg += get_type_name(t.column(0).type());
     logger->info(msg);
-    return std::make_unique<cudf::process_range>("single_column_sort", nvtx3::rgb(255,105,180));
+    return std::make_unique<cudf::process_range>("single_column_sort",
+                                                 nvtx3::rgb(255, 105, 180));
   }
 
   return nullptr;
